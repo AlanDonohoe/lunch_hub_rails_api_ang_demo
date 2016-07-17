721843d9a9365bdf803d8cb3c716ea21b7973045
Alan Donohoe <alandonohoe@hotmail.com>
2016-07-17 11:33:39 +0100

    add groups resource and specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # http://localhost:3000/group
+  # resources :groups
+  
+  # We actually want to be able to do http://localhost:3000/api/groups 
+  scope '/api' do
+    resources :groups, except: [:new, :edit]
+  end
 end
